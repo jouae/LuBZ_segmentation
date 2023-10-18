28c89199a3b9f782bfa70cf2e04ba69436de4d75
YanJie <51120603+jouae@users.noreply.github.com>
2023-10-19 06:01:55 +0800

    fix: fix the division by zero problem
    When the gradient at a certain position in phi is zero,
    using the curvature formula can lead to a division by zero situation.
    
    A filtering approach is employed to calculate curvature in non-zero regions.

--- LuBZ_segmentation.py
+++ LuBZ_segmentation.py
@@ -10,8 +10,10 @@ def LuBZ(file_path):
 
     # normlize
     data = image/(np.max(image)-np.min(image))
+
+    # np.savetxt('text.txt', data)
     
-    max_iter = 100
+    max_iter = 20
 
     # construct the inital curve
     row, col = image.shape
@@ -19,11 +21,12 @@ def LuBZ(file_path):
     x, y = np.arange(0, row), np.arange(0, col)
 
     X, Y = np.meshgrid(x, y)
-    cell_size = np.pi / 5
+    cell_size = np.pi / 10
     init_curve = cell_size * (np.sin(X) + np.sin(Y))
-    # init_curve = np.ones((row,col))*-1
-    # init_curve[200-30:150+30,200-30:150+30] *= -1
-    
+
+    # init_curve = np.ones((row,col))
+    # init_curve[185:285, 140:340] *= -1
+
     zero_level_width = 1e-3 # determind the contour width
     phi = init_curve
     phi = phi/(np.max(phi)-np.min(phi))
@@ -39,9 +42,9 @@ def LuBZ(file_path):
     edge_constraint = 1 / (1+gradient_square)
 
     # coef for delta function
-    eps_delta = 1e-3
+    eps_delta = 1e-1
     # coef for heaviside function
-    eps_heaviside = 1e-3
+    eps_heaviside = 1e-1
 
     # coef force term
     lambda1 = 1
@@ -76,7 +79,8 @@ def LuBZ(file_path):
         phi_xx = np.gradient(phi_x)[1]
         phi_yy = np.gradient(phi_y)[0]
         phi_xy = np.gradient(phi_x)[0]
-        curvature = (phi_xx * phi_y**2 + phi_yy * phi_x**2 - 2*phi_xy * phi_y*phi_x) / ((phi_x**2 + phi_y**2)**(3/2))
+        curvature = (phi_xx * phi_y**2 + phi_yy * phi_x**2 - 2*phi_xy * phi_y*phi_x)
+        curvature[(phi_x!=0)&(phi_y!=0)] /= ((phi_x[(phi_x!=0)&(phi_y!=0)]**2 + phi_y[(phi_x!=0)&(phi_y!=0)]**2)**(3/2))
 
         # Dirac delta
         dirac_delta = eps_delta/np.pi/(prev_phi**2+eps_delta**2)
@@ -101,48 +105,55 @@ def LuBZ(file_path):
 
         # full time [1, (27)]
         phi = phi_half + time_step * laplacian
-        phi[abs(phi) < zero_level_width] = 0 # narrow the cell
 
         # update
         # determind the average of each region
-        c1 = np.sum(data[phi>0]*edge_constraint[phi>0])/np.sum(edge_constraint[phi>0])
-        c2 = np.sum(data[phi<0]*edge_constraint[phi<0])/np.sum(edge_constraint[phi<0])
+        c1 = np.sum(data[phi>0]*edge_constraint[phi>0])/(np.sum(edge_constraint[phi>0]))
+        c2 = np.sum(data[phi<0]*edge_constraint[phi<0])/(np.sum(edge_constraint[phi<0]))
 
         # if np.sqrt(((phi-prev_phi)**2).mean()) < tol:
             # break
-        # if t%20==0:
-        #     plt.figure(f'{t} on image')
-        #     plt.imshow(image, cmap = plt.cm.bone)
-        #     plt.contour(phi, [0], colors='r', linewidths=0.5)
-        #     plt.title('t contour on image')
-        #     print(f'iter: {t}')
-        #     print(f'error: {np.sqrt(((phi-prev_phi)**2).mean())}')
+        if t%10==0:
+            plt.figure(f'{t} on image')
+            plt.imshow(image, cmap = plt.cm.bone)
+            plt.contour(phi, [0], colors='r', linewidths=0.5)
+            plt.title('t contour on image')
+            print(f'iter: {t}')
+            print(f'error: {np.sqrt(((phi-prev_phi)**2).mean())}')
 
         
     # plot the result
-    plt.figure('initial image')
-    plt.imshow(image)
-    plt.title('Original image')
+    fig, ax = plt.subplots(2,2)
+    ax[0,0].imshow(image)
+    ax[0,0].contour(init_curve, [0], colors='r', linewidths=0.5)
+    ax[0,0].set_title('Original image with initial curve')
+    ax[0,0].axis('off')
 
-    plt.figure('contour on image')
-    plt.imshow(image, cmap = plt.cm.bone)
-    plt.contour(phi, [0], colors='r', linewidths=0.5)
-    plt.title('Final contour on image')
+    ax[0,1].imshow(image, cmap = plt.cm.bone)
+    ax[0,1].contour(phi, [0], colors='r', linewidths=0.5)
+    ax[0,1].set_title(f'After {max_iter} iteration')
+    ax[0,1].axis('off')
 
-    plt.figure('region1 on image')
-    plt.imshow((phi>0).astype(int)*1, cmap = plt.cm.bone)
-    plt.title('region1')
+    ax[1,0].imshow((phi>0).astype(int)*1, cmap = plt.cm.bone)
+    ax[1,0].set_title('region1')
+    ax[1,0].axis('off')
 
-    plt.figure('region2 on image')
-    plt.imshow((phi<0).astype(int)*1, cmap = plt.cm.bone)
-    plt.title('region2')
+    ax[1,1].imshow((phi<0).astype(int)*1, cmap = plt.cm.bone)
+    ax[1,1].set_title('region2')
+    ax[1,1].axis('off')
 
-    plt.figure('Energy')
-    plt.plot( energy_storage, '*-')
-    plt.ylabel('energy')
-    plt.xlabel('time') 
-    plt.title('Energy at each time')
+    # plt.figure('Energy')
+    # plt.plot( energy_storage, '*-')
+    # plt.ylabel('energy')
+    # plt.xlabel('time') 
+    # plt.title('Energy at each time')
 
+
+    # Plot the surface.
+    from matplotlib import cm
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(X, Y, phi, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
     plt.show()
     return 0
 
